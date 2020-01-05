1200be102fca55fac7720d24a9048f67d12f2d78
Michael Pohl <contact@michaelpohl.de>
2020-01-05 09:57:10 +0100

    Add FFMpeg

--- app/src/main/cpp/StorageDataSource.cpp
+++ app/src/main/cpp/StorageDataSource.cpp
@@ -19,10 +19,10 @@
 
 #include "StorageDataSource.h"
 
-//#if !defined(USE_FFMPEG)
-//#error USE_FFMPEG should be defined in app.gradle
-//#endif
-//
+#if !defined(USE_FFMPEG)
+#error USE_FFMPEG should be defined in app.gradle
+#endif
+
 //#if USE_FFMPEG==1
 //#include "FFMpegExtractor.h"
 //#else
@@ -30,6 +30,7 @@
 #include "Constants.h"
 #include "logging.h"
 #include "StorageDataSource.h"
+#include "FFMpegExtractor.h"
 #include <fstream>
 
 //#endif
@@ -70,31 +71,31 @@ StorageDataSource *StorageDataSource::newFromCompressedAsset(
     // size of the decoded data until after decoding so we make an assumption about the
     // maximum compression ratio and the decoded sample format (float for FFmpeg, int16 for NDK).
 //#if USE_FFMPEG==true
-//    const long maximumDataSizeInBytes = kMaxCompressionRatio * assetSize * sizeof(float);
-//    auto decodedData = new uint8_t[maximumDataSizeInBytes];
-//
-//    int64_t bytesDecoded = FFMpegExtractor::decode(asset, decodedData, targetProperties);
-//    auto numSamples = bytesDecoded / sizeof(float);
-//#else
-    const long maximumDataSizeInBytes = kMaxCompressionRatio * assetSize * sizeof(int16_t);
+    const long maximumDataSizeInBytes = kMaxCompressionRatio * assetSize * sizeof(float);
     auto decodedData = new uint8_t[maximumDataSizeInBytes];
 
-    int64_t bytesDecoded = NDKExtractor::decode(filename, decodedData, targetProperties);
-    auto numSamples = bytesDecoded / sizeof(int16_t);
+    int64_t bytesDecoded = FFMpegExtractor::decode2(asset, decodedData, targetProperties);
+    auto numSamples = bytesDecoded / sizeof(float);
+//#else
+//    const long maximumDataSizeInBytes = kMaxCompressionRatio * assetSize * sizeof(int16_t);
+//    auto decodedData = new uint8_t[maximumDataSizeInBytes];
+//
+//    int64_t bytesDecoded = NDKExtractor::decode(filename, decodedData, targetProperties);
+//    auto numSamples = bytesDecoded / sizeof(int16_t);
 //#endif
 
     // Now we know the exact number of samples we can create a float array to hold the audio data
     auto outputBuffer = std::make_unique<float[]>(numSamples);
 
 //#if USE_FFMPEG==1
-//    memcpy(outputBuffer.get(), decodedData, (size_t)bytesDecoded);
-//#else
-    // The NDK decoder can only decode to int16, we need to convert to floats
-    oboe::convertPcm16ToFloat(
-            reinterpret_cast<int16_t *>(decodedData),
-            outputBuffer.get(),
-            bytesDecoded / sizeof(int16_t));
-//#endif
+    memcpy(outputBuffer.get(), decodedData, (size_t)bytesDecoded);
+////#else
+//    // The NDK decoder can only decode to int16, we need to convert to floats
+//    oboe::convertPcm16ToFloat(
+//            reinterpret_cast<int16_t *>(decodedData),
+//            outputBuffer.get(),
+//            bytesDecoded / sizeof(int16_t));
+////#endif
 
     delete[] decodedData;
 
